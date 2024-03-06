3e0cedbb6257e0cc443853a2d736c846e5c55ef5
Naveen Mathew <naveenmathew1585@gmail.com>
2024-03-06 23:34:25 +0530

    ♻️ split into pages

--- app.vue
+++ app.vue
@@ -1,16 +1,6 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <UCard>
-      <div class="flex">
-        <div class="flex items-center justify-center">
-          <Icon name="ph:hammer-duotone" class="text-primary" size="40px" />
-        </div>
-        <h1 class="text-5xl font-semibold ml-5">Workify</h1>
-      </div>
-      <template #footer>
-        <p>Log and Visualize your work hours</p>
-      </template>
-    </UCard>
+  <div>
+    <NuxtPage />
   </div>
 </template>
 
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <UCard>
+            <div class="flex">
+                <div class="flex items-center justify-center">
+                    <Icon name="ph:hammer-duotone" class="text-primary" size="40px" />
+                </div>
+                <h1 class="text-5xl font-semibold ml-5">Workify</h1>
+            </div>
+            <template #footer>
+                <p>Log and Visualize your work hours</p>
+            </template>
+        </UCard>
+    </div>
+</template>
