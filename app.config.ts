7fe0a48f450630bf3180a06d373ce51d9c299b67
Naveen Mathew <naveenmathew1585@gmail.com>
2024-03-06 22:44:37 +0530

    🎉 start workify

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+    nuxtIcon: {
+        size: '30px'
+    },
+    ui:{
+        primary: 'violet',
+        gray: 'cool'
+    }
+})
---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <UCard>
+      <div class="flex">
+        <div class="flex items-center justify-center">
+          <Icon name="ph:hammer-duotone" class="text-primary" size="40px" />
+        </div>
+        <h1 class="text-5xl font-semibold ml-5">Workify</h1>
+      </div>
+      <template #footer>
+        <p>Log and Visualize your work hours</p>
+      </template>
+    </UCard>
+  </div>
+</template>
+
+<style>
+@import url("https://api.fontshare.com/v2/css?f[]=space-grotesk@1,300,700,500,400,600&display=swap");
+
+body {
+  font-family: 'Space Grotesk', sans-serif;
+}
+</style>
