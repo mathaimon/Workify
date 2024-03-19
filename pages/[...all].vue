9d63758981701caed4511922221c3d3e94137262
Naveen Mathew <naveenmathew1585@gmail.com>
2024-03-19 08:44:28 +0530

    ✨ add 404 page

---
+++ pages/[...all].vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col gap-5 justify-center items-center">
+        <p class="font-semibold text-4xl">Oops!</p>
+        <p class="text-2xl text-center">The page you are looking for
+            does not exist.</p>
+
+        <UButton size="lg" to="/" variant="soft">Go Home</UButton>
+    </div>
+</template>
