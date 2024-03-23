99931bda5785a811645cb92ba3e058e3d304a083
Naveen Mathew <naveenmathew1585@gmail.com>
2024-03-23 23:09:41 +0530

    ♻️ refactored for better readablity

--- components/WorkHourDetailCard.vue
+++ components/WorkHourDetailCard.vue
@@ -1,20 +1,19 @@
 <script setup>
 const props = defineProps({
-    clockedInTime: String,
-    clockedOutTime: String
+    workHour: Object
 })
 
 const hours = ref(0)
 const minutes = ref(0)
 
 onMounted(() => {
-    const elapsedTime = new Date(props.clockedOutTime) - new Date(props.clockedInTime)
+    const elapsedTime = new Date(props.workHour.clockedOutTime) - new Date(props.workHour.clockedInTime)
     hours.value = Math.floor(elapsedTime / (1000 * 60 * 60));
     minutes.value = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
 })
 
-const formatedDate = useDateFormat(new Date(props.clockedInTime), "D MMM")
-const formatedWeekday = useDateFormat(new Date(props.clockedInTime), "dddd")
+const formatedDate = useDateFormat(new Date(props.workHour.clockedInTime), "D MMM")
+const formatedWeekday = useDateFormat(new Date(props.workHour.clockedInTime), "dddd")
 </script>
 <template>
     <UCard class="w-full max-w-md">
--- pages/work-hours/index.vue
+++ pages/work-hours/index.vue
@@ -2,6 +2,11 @@
 const workHours = useWorkHours()
 const allWorkHours = workHours.allWorkHours
 
+const router = useRouter()
+const openId = () => {
+    router.push({ name: 'work-hours-id', params: { id: '44444444' } })
+}
+
 onMounted(() => {
     workHours.listWorkHours()
 })
@@ -10,8 +15,8 @@ onMounted(() => {
     <div class="flex flex-col items-center justify-center pt-5">
         <h1 class="text-3xl font-semibold text-center">Work Hours</h1>
         <div class="flex flex-col mt-10 w-full items-center justify-center gap-5">
-            <WorkHourDetailCard v-for="workHour in allWorkHours" :clocked-in-time="workHour.clockedInTime"
-                :clocked-out-time="workHour.clockedOutTime" />
+            <UButton @click="openId">Open Route</UButton>
+            <WorkHourDetailCard v-for="workHour in allWorkHours" :workHour="workHour" />
         </div>
     </div>
 </template>
