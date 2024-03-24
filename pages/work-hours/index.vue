3c1678866f486dc9006fdc0a867eee49d1c0f874
Naveen Mathew <naveenmathew1585@gmail.com>
2024-03-24 13:13:09 +0530

    ✨ add loading animation

--- pages/work-hours/index.vue
+++ pages/work-hours/index.vue
@@ -2,6 +2,8 @@
 const workHours = useWorkHours()
 const allWorkHours = workHours.allWorkHours
 
+const showSkeletonLoader = workHours.isLoading
+
 onMounted(() => {
     workHours.listWorkHours()
 })
@@ -10,6 +12,16 @@ onMounted(() => {
     <div class="flex flex-col items-center justify-center pt-5">
         <h1 class="text-3xl font-semibold text-center">Work Hours</h1>
         <div class="flex flex-col mt-10 w-full items-center justify-center gap-5">
+            <UCard class="w-full max-w-md" v-if="showSkeletonLoader">
+                <div class="flex justify-between items-center px-3 animate-pulse">
+                    <div class="flex flex-col items-start gap-2">
+                        <div class="h-6 w-16 bg-gray-200 dark:bg-gray-700 rounded-md"></div>
+                        <div class="h-4 w-20 bg-gray-200 dark:bg-gray-700 rounded-md"></div>
+                    </div>
+                    <div class="w-20 h-10 bg-gray-200 dark:bg-gray-700 rounded-md">
+                    </div>
+                </div>
+            </UCard>
             <WorkHourDetailCard v-for="workHour in allWorkHours" :workHour="workHour" />
         </div>
     </div>
